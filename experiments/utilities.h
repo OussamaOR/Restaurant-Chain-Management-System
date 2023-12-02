910fa66b4464ccbbd6140e70e3fd2f59d46bc010
OussamaOR <mohamed.oussama.ould.rouis@ensia.edu.dz>
2023-12-02 12:15:46 +0100

    Added Project Main Structure

---
+++ experiments/utilities.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+unsigned generate_random_interv(unsigned x, unsigned y);     
+void fill_vector(std::vector<unsigned>& vec, unsigned low, unsigned high);
