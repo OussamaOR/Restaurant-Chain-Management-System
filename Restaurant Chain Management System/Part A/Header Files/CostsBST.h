bdc3356cec98dd40e3828839a332120282c3332f
manel aits <143260378+Minoxxuuyy@users.noreply.github.com>
2023-12-07 23:32:03 +0100

    Update CostsBST.h

--- Restaurant Chain Management System/Part A/Header Files/CostsBST.h
+++ Restaurant Chain Management System/Part A/Header Files/CostsBST.h
@@ -1,25 +1,6 @@
 #ifndef COSTSBST_H
 #define COSTSBST_H
-#include "date.h"
-enum costType
-{
-    rent,
-    employeePayments,
-    electricity,
-    gas,
-    vegetables,
-    meats,
-    otherIngredients,
-    publicitySpending
-};
-class dailyCost
-{
-    Date date;
-    costType costType[8];
-    // getters
-    // setters
-    // function to retun total daily cost (summing all the elements of the array)
-};
+
 class costsBST
 {
     dailyCost *root;
