c2784e4755dbe939dcebc046cbda1de6a4ce42b6
manel aits <143260378+Minoxxuuyy@users.noreply.github.com>
2023-12-07 23:34:44 +0100

    Update RatingsBST.h

--- Restaurant Chain Management System/Part A/Header Files/RatingsBST.h
+++ Restaurant Chain Management System/Part A/Header Files/RatingsBST.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "MonthlyRating.h"
 
-class RatingsBST{
-    monthlyRating root;  
+class ratingsBST
+{
+    monthlyRating root;
+    // insertion
+    // deletion + useful funcions for BST (ISEMPTY() , CONTAINS()..,ETC)
+    //function to return  the max and min rating
+    //function to retun the average rating for a given year (summing all the ratings and deviding by 12)
+
 };
