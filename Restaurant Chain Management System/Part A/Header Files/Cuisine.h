0e3c3959bf28f8bbfc5266bd52d218722244bd77
manel aits <143260378+Minoxxuuyy@users.noreply.github.com>
2023-12-07 23:35:48 +0100

    Update Cuisine.h

--- Restaurant Chain Management System/Part A/Header Files/Cuisine.h
+++ Restaurant Chain Management System/Part A/Header Files/Cuisine.h
@@ -1,21 +1,32 @@
 #pragma once
-#include <iostream>
-#include "SalesBST.h"
-#include "RatingsBST.h"
+#ifndef CUISINE_H
+#define CUISINE_H
+#include "ratingsBST.h"
+#include "salesBST.h"
 
-enum CuisineType
+
+// THE TYPE OF THE CUISINE
+enum class CuisineType
 {
-    ALGERIAN = 1,
+    ALGERIAN,
     SYRIAN,
     CHINESE,
     INDIAN,
     EUROPEAN
 };
 
+
+//THE CUISINE CLASS
 class Cuisine
 {
 private:
     CuisineType cuisineType;
-    SalesBST dailySales;
-    RatingsBST monthlyRating;
+    salesBST dailySales;
+    ratingsBST monthlyRating;
+    // function of total dailysales
 };
+#endif
+
+//function to return the monthlysales (summing up all the daily sales for a given month) ==> month o(logn)
+//function to return the total sales of a cuisine (summing up all the daily sales) ==> year
+//function to display the prize winners for the 5 cuisines on a given month 
