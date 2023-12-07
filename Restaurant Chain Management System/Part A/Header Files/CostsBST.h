5807ec98b50af987dd05547d2b2d7bc3577fa078
manel aits <143260378+Minoxxuuyy@users.noreply.github.com>
2023-12-07 23:30:58 +0100

    Update CostsBST.h

--- Restaurant Chain Management System/Part A/Header Files/CostsBST.h
+++ Restaurant Chain Management System/Part A/Header Files/CostsBST.h
@@ -1,6 +1,33 @@
-#pragma once
-#include "DailyCost.h"
-
-class CostsBST{
-    DailyCost root;  
+#ifndef COSTSBST_H
+#define COSTSBST_H
+#include "date.h"
+enum costType
+{
+    rent,
+    employeePayments,
+    electricity,
+    gas,
+    vegetables,
+    meats,
+    otherIngredients,
+    publicitySpending
 };
+class dailyCost
+{
+    Date date;
+    costType costType[8];
+    // getters
+    // setters
+    // function to retun total daily cost (summing all the elements of the array)
+};
+class costsBST
+{
+    dailyCost *root;
+    // isertion
+    // deletion + useful funcions for BST (ISEMPTY() , CONTAINS()..,ETC)
+    //function to return min and max (daily sale) (you can add it for the max sale on a month if you see it's useful)
+    // function to return costs on a specific period
+    // function to return total costs on a month
+    // function to return total costs on a year
+};
+#endif
