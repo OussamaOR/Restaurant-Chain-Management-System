1179a334a455780f3807f2c9ec850e92eaba9783
manel aits <143260378+Minoxxuuyy@users.noreply.github.com>
2023-12-08 10:37:02 +0100

    Update Date.h

--- Restaurant Chain Management System/Part A/Header Files/Date.h
+++ Restaurant Chain Management System/Part A/Header Files/Date.h
@@ -4,6 +4,12 @@ private:
     int day;
     int month;
     int year;
+    bool isValidDay(int , int , int ) const;
+    bool isValidMonth(int ) const;
+    bool isValidYear(int ) const;
+    void validateDate();
+    int daysInMonth(int , int ) const;
+    bool isLeapYear(int ) const;
 public:
 Date();
 Date(int,int,int);
