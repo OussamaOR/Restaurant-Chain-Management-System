da41db0f28628cbe6f4952f8aea6d889a89ab7c2
OussamaOR <mohamed.oussama.ould.rouis@ensia.edu.dz>
2023-12-08 15:32:51 +0100

    bst class

--- Restaurant Chain Management System/Part A/Utilities Files/BinarySearchTree.h
+++ Restaurant Chain Management System/Part A/Utilities Files/BinarySearchTree.h
@@ -1,8 +1,24 @@
 
-template<typename Comparable>
+template<typename Object>
 class BinarySearchTree {
     private:
-
+    struct Node {
+        Object data;
+        Node* right;
+        Node* left;
+        Node(const Object& obj):data(obj),left(0),right(0){}
+    };
+    Node* root;
     public:
-    
+    BinarySearchTree();
+    ~BinarySearchTree();
+    void insert(const Object& );
+    void remove(const Object& );
+    void printInOrder() const;
+    private:
+    Node* insertHelper(Node* root, const Object& value);
+    Node* removeHelper(Node* root, const Object& value);
+    Node* findMin(Node* root) const;
+    void printInOrderHelper(Node* root) const;
+    void destroyTree(Node* root);
 };
