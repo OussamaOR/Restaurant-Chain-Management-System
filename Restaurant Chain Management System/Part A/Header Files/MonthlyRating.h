9c14ec5a20d41b51b4246009c6be9632f985ef67
ludi <146668364+ludmila-sahraoui@users.noreply.github.com>
2023-12-10 09:44:56 +0100

    Update MonthlyRating.h

--- Restaurant Chain Management System/Part A/Header Files/MonthlyRating.h
+++ Restaurant Chain Management System/Part A/Header Files/MonthlyRating.h
@@ -1,14 +1,30 @@
-#pragma once
-#include <iostream>
+#ifndef MONTHRATING_H
+#define MONTHRATING_H
+#include <utility>
 #include <vector>
-#include "Date.h"
-using namespace std;
 
-struct monthlyRating
+class MonthRating
 {
-    pair<int, int> date;
-    vector<int> ratings;
-    // getters
-    // setters
-    // function that returns the average rating (summing all the elements of the vector / size of the vector)
+    public:
+        MonthRating( int , int );
+        virtual ~MonthRating();
+        void set_RatingDates(int , int );
+        std::pair<int, int> get_RatingDates();
+        void print_RatingDates();
+        void insert_dayrating(int n);
+        void printdilyrating() const;
+        float averagerating();
+        bool operator<(const MonthRating&)const ;
+        bool operator>(const MonthRating&)const ;
+        bool operator==(const MonthRating&)const ;
+        bool operator!=(const MonthRating&)const ;
+        bool operator<=(const MonthRating&)const ;
+        bool operator>=(const MonthRating&)const ;
+    protected:
+     std::pair<int , int> ratingdate;
+     std::vector<int > ratings;
+     std::size_t dayindex;
+     std::size_t maxsize;
 };
+
+#endif // MONTHRATING_H
