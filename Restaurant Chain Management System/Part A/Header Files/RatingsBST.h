253110ef6b0bfb3e2f81f141c92c37c640ca9574
alia000uwu <gimme.oreo010@gmail.com>
2023-12-15 20:45:17 +0100

    header file for the rating bst update new functions

--- Restaurant Chain Management System/Part A/Header Files/RatingsBST.h
+++ Restaurant Chain Management System/Part A/Header Files/RatingsBST.h
@@ -1,5 +1,5 @@
 #include "MonthlyRating.h"
-#include "../Source Files/BinarySearchTree.cpp"
+//#include "../Source Files/BinarySearchTree.cpp"
 #ifndef RATINGSBST_H
 #define RATINGSBST_H
 
@@ -16,24 +16,26 @@ public:
     void remove(std::pair<int,int> , std::vector<int>);
     void print(MonthlyRating*);
     bool makeEmpty(MonthlyRating* );
+    MonthlyRating* getRoot() const {return root;}
 
     //functions used when print the needed results
     float getAverageRatingByMonth(MonthlyRating*,int , int);//int month and int year;
     float getAverageRatingByMonth(MonthlyRating*,std::pair<int,int>);
-
     float getAverageratingByYear(MonthlyRating*,int);
-
     float getAverageratingByPeriod(MonthlyRating*,std::pair<int,int>,std::pair<int,int>);
+    
     private:
 
     MonthlyRating *root;
+
     //helper functions
     MonthlyRating *insertHelper(MonthlyRating *, std::pair<int,int> , std::vector<int>);
     MonthlyRating *removeHelper(MonthlyRating *,std::pair<int,int> , std::vector<int>);
     MonthlyRating *findMin(MonthlyRating *) const;
-    float getAverageRatingByMonthHelper(MonthlyRating* ,int , int);
-    float getAverageratingByYearHelper(MonthlyRating*,int);
-    float getAverageratingByPeriodHelper(MonthlyRating*,std::pair<int,int>,std::pair<int,int>);
+    double getAverageRatingByMonthHelper(MonthlyRating* ,std::pair<int,int>);
+    double getAverageratingByYearHelper(MonthlyRating*,int  );
+    double getAverageratingByPeriodHelper(MonthlyRating*,std::pair<int,int>,std::pair<int,int>);
+    bool checkDate(std::pair<int,int>);
     void printHelper(MonthlyRating *) const;
 };
 
