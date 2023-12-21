c594b92be77eb5262e5221371f23da660f0a4ebc
manel ait said <manelaitsaid12@gmail.com>
2023-12-21 10:27:10 +0100

    modifications

--- Restaurant Chain Management System/Part A/Header Files/readfiles.h
+++ Restaurant Chain Management System/Part A/Header Files/readfiles.h
@@ -3,15 +3,15 @@
 #include <sstream>
 #include <vector>
 #include <regex>
-Date parseDate(const std::string &dateString);
+Date parseDate(const std::string &);
 
 // Function to parse SalesVec from a string
-SalesVec parseSalesVec(const std::string &salesVecString);
+SalesVec parseSalesVec(const std::string &);
 
 // function to read data from costs.csv file
-void readCSVAndInsertIntoCostsVec(const std::string &filename, CostsVec &costsVec,int restaurantIndex);
+void readCSVAndInsertIntoCostsVec(const std::string &, CostsVec &,int );
 
 // function to read sata from cuisine.csv file
-Cuisine readCuisineFromCSV(const std::string &filePath, int cuisineIndex);
+Cuisine readCuisineFromCSV(const std::string &, int );
 // function to read data from the restaurant csv file
-void readRestaurantCSV(const std::string &filename, RestaurantHashTable &restaurantTable);
+void readRestaurantCSV(const std::string &, RestaurantHashTable &);
