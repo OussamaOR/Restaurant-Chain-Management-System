ae37a068d326df4ccb11fe318e8ef59da77bc905
Roumaissa Mekhoukh <146542059+RoumaissaMkh@users.noreply.github.com>
2023-12-23 15:46:51 +0100

    Update SalesVec.cpp adding comments

--- Restaurant Chain Management System/Part B/Source Files/SalesVec.cpp
+++ Restaurant Chain Management System/Part B/Source Files/SalesVec.cpp
@@ -1,33 +1,52 @@
 #include "../Header Files/SalesVec.h"
 #include "../Header Files/CostsVec.h"
-//struct DailySale
 
+// Definitions for the DailySale struct
+
+// Default constructor
 DailySale::DailySale() : dailySaleAmount(0.0) {}
+
+// Destructor
 DailySale::~DailySale() {}
-// Constructor with parameters 
+
+// Constructor with parameters
 DailySale::DailySale(const Date &date, double dailySaleAmount)
     : date(date), dailySaleAmount(dailySaleAmount) {}
 
-// Copy constructor 
+// Copy constructor
 DailySale::DailySale(const DailySale &other)
     : date(other.date), dailySaleAmount(other.dailySaleAmount) {}
 
-// Move constructor 
+// Move constructor
 DailySale::DailySale(DailySale &&other)
     : date(other.date), dailySaleAmount(other.dailySaleAmount)
 {
      other.dailySaleAmount = 0.0;
 }
+
+// Definitions for the SalesVec class
+
+// Constructor with rvalue reference for efficient move semantics
 SalesVec::SalesVec(std::vector<DailySale>&& salesVector) : Salesvec(std::move(salesVector)) {}
+
+// Default constructor
 SalesVec::SalesVec(){}
+
+// Destructor
 SalesVec::~SalesVec(){}
+
+// Method to insert a DailySale object into the SalesVec
 void SalesVec::insert_dailysale(DailySale d){
     Salesvec.push_back(d);
 }
-int SalesVec::getDailySaleIndex(int day = 1, int month = 1, int year = 0)
+
+// Method to get the index of a specific day in the SalesVec
+int SalesVec::getDailySaleIndex(int day, int month, int year)
 {
     return (year - Salesvec[0].getDate().getYear()) * 365 + (month - Salesvec[0].getDate().getMonth()) * 30 + day - Salesvec[0].getDate().getDay();
 }
+
+// Method to find the maximum sale amount in the SalesVec
 float SalesVec::max_sale()
 {
     float current = 0.f;
@@ -38,6 +57,8 @@ float SalesVec::max_sale()
     }
     return current;
 }
+
+// Method to find the minimum sale amount in the SalesVec
 float SalesVec::min_sale()
 {
     float current = static_cast<float>(INT32_MAX);
@@ -48,6 +69,8 @@ float SalesVec::min_sale()
     }
     return current;
 }
+
+// Method to calculate the total sales for a specific month and year
 float SalesVec::total_onmonth(int month, int year)
 {
     float total = 0.f;
@@ -59,6 +82,8 @@ float SalesVec::total_onmonth(int month, int year)
     }
     return total;
 }
+
+// Method to calculate the total sales for a specific year
 float SalesVec::total_onyear(int year)
 {
     float total = 0.f;
@@ -70,6 +95,8 @@ float SalesVec::total_onyear(int year)
     }
     return total;
 }
+
+// Method to calculate the total sales for a specified period of time
 float SalesVec::total_onperiod(Date start_date, Date end_date)
 {
     float total = 0.f;
@@ -81,6 +108,8 @@ float SalesVec::total_onperiod(Date start_date, Date end_date)
     }
     return total;
 }
+
+// Copy assignment operator
 SalesVec &SalesVec::operator=(const SalesVec &other)
 {
     if (this != &other)
@@ -90,14 +119,15 @@ SalesVec &SalesVec::operator=(const SalesVec &other)
     }
     return *this;
 }
+
+// Method to print the DailySale objects in the SalesVec
 void SalesVec::printSalesVec() {
-    for (int i=0;i<Salesvec.size();i++) {
+    for (int i=0; i<Salesvec.size(); i++) {
         Salesvec[i].printSale();
     }
 }
 
-
-
+// Method to print the DailySale objects for a specific month and year
 void SalesVec::printSalesOnMonth(int month, int year) {
     if (getDailySaleIndex(1, month, year) >= Salesvec.size())
         return;
